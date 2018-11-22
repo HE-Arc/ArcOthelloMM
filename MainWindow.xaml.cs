63ea826af78ae66154ff8bb5da85df418f64bb58
Fabien Mottier <fabien.mottier@he-arc.ch>
2018-11-23 00:16:47 +0100

    create the board

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,87 @@ namespace ArcOthelloMM
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int ROW = 7;
+        private const int COLUMN = 9;
+        private const int SIZE_PIECE = 30;
+        private int[,] board;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            initBoard();
+            initGame();
+        }
+
+        private void initBoard()
+        {
+            board = new int[ROW, COLUMN];
+
+            for (int i = 0; i < ROW; ++i)
+            {
+                for (int j = 0; j < COLUMN; ++j)
+                {
+
+                    board[i, j] = 0;
+                }
+            }
+        }
+
+        private void initGame()
+        {
+            addPiece(r33, false);
+            addPiece(r34, true);
+            addPiece(r43, true);
+            addPiece(r44, false);
+        }
+
+        private void addPiece(Rectangle rec, bool black)
+        {
+            Ellipse circle = new Ellipse
+            {
+                Name = "c" + rec.Name.Substring(1),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = SIZE_PIECE,
+                Height = SIZE_PIECE,
+                Fill = (black) ? Brushes.Black : Brushes.White,
+                Margin = new Thickness(rec.Margin.Left + 5, rec.Margin.Top + 5, 0, 0)
+            };
+
+            if (black)
+            {
+                circle.Fill = Brushes.Black;
+                placeOnBoard(1, rec.Name);
+            }
+            else
+            {
+                circle.Fill = Brushes.White;
+                placeOnBoard(2, rec.Name);
+            }
+
+            grid.Children.Add(circle);
+        }
+
+        private void pickPiece(string name)
+        {
+            Ellipse circle = (Ellipse)grid.FindName(name);
+
+            if (circle.Fill == Brushes.Black)
+            {
+                circle.Fill = Brushes.Black;
+                placeOnBoard(1, circle.Name);
+            }
+            else
+            {
+                circle.Fill = Brushes.White;
+                placeOnBoard(2, circle.Name);
+            }
+        }
+
+        private void placeOnBoard(int value, string position)
+        {
+            board[int.Parse(position[1].ToString()), int.Parse(position[2].ToString())] = value;
         }
     }
 }
