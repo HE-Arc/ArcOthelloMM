b2e6d93489542ebd18de6604440c6704e2bb0f10
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-01-17 18:40:25 +0100

    Begining of a new window to display game end

---
+++ ArcOthelloMM/OthelloEndOfGame.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ArcOthelloMM
+{
+    /// <summary>
+    /// Interaction logic for OthelloEndOfGame.xaml
+    /// </summary>
+    public partial class OthelloEndOfGame : Window
+    {
+        public OthelloEndOfGame(Player player)
+        {
+            InitializeComponent();
+            //msg.Content = "Joueur " + player.Name + " a gagné !";
+        }
+    }
+}
