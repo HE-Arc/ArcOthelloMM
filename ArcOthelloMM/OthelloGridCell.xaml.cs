cd5e84d7ae1aea94795696ea994ad8e24c2952d2
Fabien Mottier <fabien.mottier@he-arc.ch>
2019-01-17 13:55:05 +0100

    replace projects

---
+++ ArcOthelloMM/OthelloGridCell.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcOthelloMM
+{
+    /// <summary>
+    /// Interaction logic for OthelloGridCell.xaml
+    /// </summary>
+    public partial class OthelloGridCell : UserControl
+    {
+        public enum States
+        {
+            Empty,
+            Player1,
+            Player2,
+            PreviewPlayer1,
+            PreviewPlayer2
+        }
+
+        private States _state;
+
+        public States State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                tokenContent.Fill = colors[_state];
+                tokenContent.Width = sizes[_state];
+                tokenContent.Height = sizes[_state];
+            }
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        private bool _lastPlay;
+        public bool LastPlay
+        {
+            get
+            {
+                return _lastPlay;
+            }
+            set
+            {
+                _lastPlay = value;
+                tokenContent.Stroke = _lastPlay ? borderColorEllipseLastPlay : transparent;
+            }
+        }
+
+        public event EventHandler Click;
+
+        static Dictionary<States, Brush> colors;
+        static Dictionary<States, int> sizes;
+
+        static Brush borderColorEllipseLastPlay= BrushFromColor(Color.FromRgb(111, 255, 97), 255);
+        static Brush transparent = BrushFromColor(Color.FromRgb(0, 0, 0), 0);
+
+        static Brush backgroundColor = BrushFromColor(Color.FromRgb(0, 0, 0), 127);
+        static Brush borderColor = BrushFromColor(Color.FromRgb(255, 255, 255), 127);
+
+        static int normalSize = 100;
+        static byte normalOpacity = 220;
+
+        static int previewSize = (int)(0.75*normalSize);
+        static byte previewOpacity = (byte)(0.25 * normalOpacity);
+
+
+        static OthelloGridCell()
+        {
+            colors = new Dictionary<States, Brush>();
+            colors.Add(States.Empty, BrushFromColor(Color.FromRgb(0,0,0), 0));
+            colors.Add(States.Player1, BrushFromColor(Player.WhitePlayer.Color2, normalOpacity));
+            colors.Add(States.Player2, BrushFromColor(Player.BlackPlayer.Color2, normalOpacity));
+            colors.Add(States.PreviewPlayer1, BrushFromColor(Player.WhitePlayer.Color2, previewOpacity));
+            colors.Add(States.PreviewPlayer2, BrushFromColor(Player.BlackPlayer.Color2, previewOpacity));
+            sizes = new Dictionary<States, int>();
+            sizes.Add(States.Empty, 0);
+            sizes.Add(States.Player1, normalSize);
+            sizes.Add(States.Player2, normalSize);
+            sizes.Add(States.PreviewPlayer1, previewSize);
+            sizes.Add(States.PreviewPlayer2, previewSize);
+        }
+
+        static SolidColorBrush BrushFromColor(Color color, byte alpha)
+        {
+            return new SolidColorBrush(Color.FromArgb(alpha, color.R, color.G, color.B));
+        }
+
+        public OthelloGridCell(int x, int y, States states = States.Empty)
+        {
+            InitializeComponent();
+            this.State = states;
+            this.X = x;
+            this.Y = y;
+            this.LastPlay = false;
+            button.Background = backgroundColor;
+            button.BorderBrush = borderColor;
+            button.Click += ButtonClick; // Adding the click on the parent of the button
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke(this, e); // trigger the parent click event on the button (children) event trigger
+        }
+    }
+}
---
+++ IAMargueronMottier/Player.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.Serialization;
+using System.Windows.Media;
+
+namespace ArcOthelloMM
+{
+    [Serializable]
+    public class Player : ISerializable
+    {
+        public List<Tuple<int, int>> Tokens { get; set; }
+        public int Value { get; set; }
+        public string Name { get; set; }
+        public Color Color { get; }
+        public Color Color2 { get; }
+
+        private Stopwatch Stopwatch { get; set; }
+
+        private long PreviousTime { get; set; }
+
+
+        private static Player whitePlayer;
+        private static Player blackPlayer;
+
+        /// <summary>
+        /// Instanciate the white player
+        /// </summary>
+        private Player(int value, string name, Color color, Color color2)
+        {
+            Reset();
+            Value = value;
+            Name = name;
+            PreviousTime = 0;
+            Color = color;
+            Color2 = color2;
+        }
+
+        public static Player WhitePlayer
+        {
+            get
+            {
+                if (whitePlayer == null)
+                    whitePlayer = new Player(1, "Bleu", Color.FromRgb(97, 111, 255), Color.FromRgb(68, 82, 233));
+                return whitePlayer;
+            }
+            set
+            {
+                whitePlayer = value;
+            }
+        }
+
+        public static Player BlackPlayer
+        {
+            get
+            {
+                if (blackPlayer == null)
+                    blackPlayer = new Player(0, "Rouge", Color.FromRgb(255, 97, 97), Color.FromRgb(233, 68, 82));
+                return blackPlayer;
+            }
+
+            set
+            {
+                blackPlayer = value;
+            }
+        }
+
+        /// <summary>
+        /// Return the value of white player
+        /// </summary>
+        /// <returns></returns>
+        public int GetValue()
+        {
+            return Value;
+        }
+
+        public int Score
+        {
+            get
+            {
+                return Tokens.Count;
+            }
+        }
+
+        public void Reset()
+        {
+            Tokens = new List<Tuple<int, int>>();
+            Stopwatch = new Stopwatch();
+            PreviousTime = 0;
+        }
+
+        protected Player(SerializationInfo info, StreamingContext context)
+        {
+            Tokens = (List<Tuple<int, int>>)info.GetValue("Tokens", typeof(List<Tuple<int, int>>));
+            Value = (int)info.GetValue("Value", typeof(int));
+            Name = (string)info.GetValue("Name", typeof(string));
+            Stopwatch = new Stopwatch(); // not serializable
+            PreviousTime = (long)info.GetValue("PreviousTime", typeof(long)); //mini hack to avoid stopwatch not serializable
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Tokens", Tokens);
+            info.AddValue("Value", Value);
+            info.AddValue("Name", Name);
+            info.AddValue("PreviousTime", GetTime());
+        }
+
+        public long GetTime()
+        {
+            return Stopwatch.ElapsedMilliseconds + PreviousTime;
+        }
+
+        public void Start()
+        {
+            Stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            Stopwatch.Stop();
+        }
+    }
+}
