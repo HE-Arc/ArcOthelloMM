d4ac550a3edd747770ebb02eb7b84835f8f475ab
Fabien Mottier <fabien.mottier@he-arc.ch>
2019-02-15 09:29:04 +0100

    base alpha-beta

--- IAMargueronMottier/IA.cs
+++ IAMargueronMottier/IA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IAMargueronMottier;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +9,58 @@ namespace ArcOthelloMM
 {
     class IA
     {
-        public static IA Instance;
-
-        private IA()
+        /// <summary>
+        /// Property for singleton
+        /// </summary>
+        private static IA instance;
+        public static IA Instance
         {
-            
-        }
+            get
+            {
+                if (instance == null)
+                    instance = new IA();
+                return instance;
+            }
 
-        public static IA GetInstance()
-        {
-            if (Instance == null)
-                Instance = new IA();
-            return Instance;
+            set
+            {
+                instance = value;
+            }
         }
 
         public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
         {
             return null;
+        }
+
+        private Tuple<int, object> AlphaBeta(TreeNode root, int level, int minOrMax, int parentValue)
+        {
+            if (level == 0 || root.Final())
+            {
+                return new Tuple<int, object>(root.Evaluate(), null);
+            }
+
+            int optVal = minOrMax * int.MinValue;
+            object optOp = null;
+
+            foreach (var op in root.Ops())
+            {
+                TreeNode newNode = root.Apply(op);
+                Tuple<int, object> res = AlphaBeta(newNode, level - 1, minOrMax * -1, optVal);
+
+                if (res.Item1 * minOrMax > optVal * minOrMax)
+                {
+                    optVal = res.Item1;
+                    optOp = op;
+
+                    if (res.Item1 * minOrMax > parentValue * minOrMax)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return new Tuple<int, object>(root.Evaluate(), null);
         }
     }
 }
---
+++ IAMargueronMottier/TreeNode.cs
@@ -0,0 +1,42 @@
+﻿using ArcOthelloMM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAMargueronMottier
+{
+    class TreeNode
+    {
+        private List<Tuple<int, int>> ListMove { get; set; }
+        private int[,] Board { get; set; }
+
+        public TreeNode(int[,] board)
+        {
+            Board = board;
+            ListMove = new List<Tuple<int, int>>();
+        }
+
+        public int Evaluate()
+        {
+            return 0;
+        }
+
+        public bool Final()
+        {
+            return false;
+        }
+
+        public List<Tuple<int, int>> Ops()
+        {
+            return ListMove;
+        }
+
+        public TreeNode Apply(Tuple<int, int> tuple, bool isWhite)
+        {
+            LogicalBoard.Instance.PlayMove(tuple.Item1, tuple.Item2, isWhite);
+            return new TreeNode(LogicalBoard.Instance.GetBoard());
+        }
+    }
+}
